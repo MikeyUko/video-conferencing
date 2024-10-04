e84d4462550bd4ee4c8fe0006d9218ac93da2b7e
Michael Anthony <ukomikey@gmail.com>
2024-10-03 22:37:43 -0400

    basic routing implemented

---
+++ app/(root)/(home)/personal-room/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const PersonalRoom = () => {
+    return (
+        <div>PersonalRoom</div>
+    )
+}
+export default PersonalRoom
---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Recordings = () => {
+    return (
+        <div>Recordings</div>
+    )
+}
+export default Recordings
