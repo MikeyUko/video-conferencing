fbe4c5a0bc401adce2ce6471da9aedbdfdfa31b5
Michael Anthony <ukomikey@gmail.com>
2024-10-06 14:21:24 -0400

    added MeetingModal

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MeetingTypeList from "@/components/MeetingTypeList";
 
 const Home = () => {
     const now = new Date();
@@ -17,6 +18,8 @@ const Home = () => {
                     </div>
                 </div>
             </div>
+
+            <MeetingTypeList />
         </section>
     )
 }
